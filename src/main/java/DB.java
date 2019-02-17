8ef4e450b51e7dfa7ec047030fe99fbe0e61e61c
root <root@localhost.localdomain>
2019-02-18 00:28:14 +0300

    new changes

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,11 +1,14 @@
 import org.sql2o.*;
 import java.net.URI;
 import java.net.URISyntaxException;
+import org.slf4j.Logger;                       
+import org.slf4j.LoggerFactory;
 
 public class DB {
     private static URI dbUri;
     public static Sql2o sql2o;
 
+    static Logger logger = LoggerFactory.getLogger(DB.class);
     static {
 
         try {
@@ -18,11 +21,12 @@ public class DB {
             int port = dbUri.getPort();
             String host = dbUri.getHost();
             String path = dbUri.getPath();
-            String username = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[0];
-            String password = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[1];
+            String username = (dbUri.getUserInfo() == null) ? "root" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "wildlife" : dbUri.getUserInfo().split(":")[1];
 
             sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
         } catch (URISyntaxException e ) {
+            logger.error("Unable to connect to database.");  
         }
     }
 }
