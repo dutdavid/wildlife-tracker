7b060992b8760493fe1067a330e4ce6f518c8d2a
root <root@localhost.localdomain>
2019-02-19 10:18:08 +0300

    new changes

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,7 +5,7 @@ public class DatabaseRule extends ExternalResource {
 
     @Override
     protected void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "root", "wildlife");
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "root", "wildlife");
     }
 
     @Override
